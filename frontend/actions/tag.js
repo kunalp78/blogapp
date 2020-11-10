8444002d2a4fda497e1ee39b9a430130db1ab6bb
Kunal <kunalp78@gmail.com>
2020-11-10 19:23:01 +0530

    created safe auth to tags blogs and categories for admin

--- frontend/actions/tag.js
+++ frontend/actions/tag.js
@@ -1,6 +1,6 @@
 import fetch from 'isomorphic-fetch';
 import {API} from '../config';
-
+import { isAuth, handleResponse} from './auth';
 export const create = (tag, token) =>{
     
     return fetch(`${API}/tag`,{
@@ -13,6 +13,7 @@ export const create = (tag, token) =>{
         body: JSON.stringify(tag)
     })
    .then( response=> {
+        handleResponse(response);
        return response.json()})
 .catch(e=>{
     console.log(e)
@@ -51,6 +52,7 @@ export const removeTag = (slug, token) =>{
         }
     })
    .then( response=> {
+        handleResponse(response);
        return response.json()})
 .catch(e=>{
     console.log(e)
