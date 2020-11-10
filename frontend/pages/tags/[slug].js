81a465a7806c3cfa4eade68a81f4d9030840db93
Kunal <kunalp78@gmail.com>
2020-11-10 15:03:37 +0530

    meta tag for tags page

--- frontend/pages/tags/[slug].js
+++ frontend/pages/tags/[slug].js
@@ -8,9 +8,26 @@ import renderHTML from 'react-render-html';
 import moment from 'moment';
 import Card from '../../components/blog/Card';
 
-const Tag = ({ tag, blogs }) => {
+const Tag = ({ tag, blogs, query }) => {
     return (
         <React.Fragment>
+        <Head>
+            <title>{tag.name} | {APP_NAME}</title>
+            <meta name="description" content={`Daily News Analysis ${tag.name}`}/>
+            <link rel="canonical" href={`${DOMAIN}/tags/${query.slug}`} />
+            <meta property="og:title" content={`${tag.name} | ${APP_NAME}`}/>
+            <meta property="og:description" content={`Daily News Analysis ${tag.name}`}/>
+            <meta property="og:type" content="website"/>
+            <meta property="og:url" content={`${DOMAIN}/tags/${query.slug}`}
+            />
+            <meta property="og:site_name" content={`${APP_NAME}`}
+            />
+            <meta property="og:image" content={`${DOMAIN}/static/images/newsapp.jpg`}/>
+            <meta property="og:image:secure_url" content={`${DOMAIN}/static/images/newsapp.jpg`}/>
+            <meta property="og:image:type" content="image/jpg"/>
+            <meta property="fb:app_id" content={`${FB_APP_ID}`}/>
+
+        </Head>
             <Layout>
                 <main>
                     <div className="container-fluid text-center">
